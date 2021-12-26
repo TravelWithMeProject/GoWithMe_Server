9a7126273f69581f6f64bac1915952d857dcfc10
ilgolf <ilgolc@naver.com>
2021-12-26 10:43:30 +0900

    initial commit

---
+++ src/test/java/team/backend/trip/TripApplicationTests.java
@@ -0,0 +1,13 @@
+package team.backend.trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
