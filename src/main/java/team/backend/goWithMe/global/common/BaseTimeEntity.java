9853bb9f1b02ccb8a0f21f7cb7f2b94714d5d108
ilgolf <ilgolc@naver.com>
2021-12-29 23:43:21 +0900

    "JPA Audit Config 추가"

---
+++ src/main/java/team/backend/goWithMe/global/common/BaseTimeEntity.java
@@ -0,0 +1,32 @@
+package team.backend.goWithMe.global.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_time", updatable = false)
+    private LocalDateTime createTime;
+
+    @LastModifiedDate
+    @Column(name = "update_time")
+    private LocalDateTime updateTime;
+
+    @Column(name = "deleted_tem")
+    private LocalDateTime deletedTime;
+
+    protected void recordDeletedTime(final LocalDateTime deletedTime) {
+        this.deletedTime = deletedTime;
+    }
+}
