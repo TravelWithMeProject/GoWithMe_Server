1c786a4998ce71186387c7834251beca4ddb4d4b
ilgolf <ilgolc@naver.com>
2022-01-03 16:54:05 +0900

    "JPA Auditing Config 파일 삭제 및 Application 클래스에 추가"

--- src/main/java/team/backend/goWithMe/TripApplication.java
+++ src/main/java/team/backend/goWithMe/TripApplication.java
@@ -2,8 +2,10 @@ package team.backend.goWithMe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TripApplication {
 
     public static void main(String[] args) {
