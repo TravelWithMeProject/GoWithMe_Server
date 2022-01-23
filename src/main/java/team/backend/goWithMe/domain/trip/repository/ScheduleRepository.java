65308be091c163aaac2b021fbf96ddab9825db8e
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2022-01-24 01:36:08 +0900

    의존성을 위한 Schedule(구체 일정) Repository 조기 구현

---
+++ src/main/java/team/backend/goWithMe/domain/trip/repository/ScheduleRepository.java
@@ -0,0 +1,21 @@
+package team.backend.goWithMe.domain.trip.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import team.backend.goWithMe.domain.trip.domain.persist.Schedule;
+import team.backend.goWithMe.domain.trip.domain.persist.TimeTable;
+
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    @Query("SELECT s FROM Schedule s WHERE s.timeTable.id =:timeTableId")
+    @Transactional(readOnly = true)
+    List<Schedule> findByTimeTableId(@Param("timeTableId") Long timeTableId);
+
+    @Query("SELECT s FROM Schedule s WHERE s.timeTable.id =:timeTableId order by s.schedulePeriod.detailStart asc")
+    @Transactional(readOnly = true)
+    List<Schedule> findByTimeTableIdOrderByPeriodStart(@Param("timeTableId") Long timeTableId);
+}
