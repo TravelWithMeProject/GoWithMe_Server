52c689231fc2621a50dd350f033651ca38431ac4
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2022-01-24 01:33:19 +0900

    Timetable 관련 DTO 구현

---
+++ src/main/java/team/backend/goWithMe/domain/trip/dto/request/TimeTableCreateDTO.java
@@ -0,0 +1,33 @@
+package team.backend.goWithMe.domain.trip.dto.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ApiModel
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class TimeTableCreateDTO {
+
+    @ApiModelProperty(value = "Member 아이디 값", required = true, example = "1")
+    private Long userId;
+
+    @ApiModelProperty(value = "시간표 명", required = true, example = "몽골 온천 여행")
+    private String timeTableName;
+
+    @ApiModelProperty(value = "시간표 내용", required = false, example = "이번 온천 여행은 몽골로 가보기로 했다.")
+    private String timeTableContent;
+
+    @ApiModelProperty(value = "여행 기간 시작 시점", required = true, example = "2022-01-01T00:00:00")
+    private LocalDateTime totalPeriodStart;
+
+    @ApiModelProperty(value = "여행 기간 끝 시점", required = true, example = "2022-01-21T13:00:00")
+    private LocalDateTime totalPeriodEnd;
+
+}
---
+++ src/main/java/team/backend/goWithMe/domain/trip/dto/response/TimeTableDTO.java
@@ -0,0 +1,33 @@
+package team.backend.goWithMe.domain.trip.dto.response;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ApiModel
+public class TimeTableDTO {
+
+    @ApiModelProperty(example = "1")
+    private long timeTableId;
+
+    @ApiModelProperty(value = "시간표 명", required = true, example = "몽골 온천 여행")
+    private String tableName;
+
+    @ApiModelProperty(value = "시간표 내용", required = false, example = "이번 온천 여행은 몽골로 가보기로 했다.")
+    private String content;
+
+    @ApiModelProperty(value = "여행 기간 시작 시점", required = true, example = "2022-01-01T00:00:00")
+    private LocalDateTime totalPeriodStart;
+
+    @ApiModelProperty(value = "여행 기간 끝 시점", required = true, example = "2022-01-21T13:00:00")
+    private LocalDateTime totalPeriodEnd;
+
+}
