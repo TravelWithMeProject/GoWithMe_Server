26955c3fd161a58b56c280c7d1d88a5995f75456
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2022-02-07 16:46:06 +0900

    Schedule Service & Controller 작업에 따른 엔티티 관련 코드 수정

--- src/main/java/team/backend/goWithMe/domain/trip/domain/vo/ScheduleTitle.java
+++ src/main/java/team/backend/goWithMe/domain/trip/domain/vo/ScheduleTitle.java
@@ -5,7 +5,7 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import team.backend.goWithMe.domain.trip.error.exception.ScheduleTitleInvalidException;
-import team.backend.goWithMe.domain.trip.error.TimeTableErrorCode;
+import team.backend.goWithMe.global.error.exception.ErrorCode;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -48,7 +48,7 @@ public final class ScheduleTitle {
 
     private static void validateScheduleTitle(String scheduleTitle) {
         if (scheduleTitle == null || scheduleTitle.isBlank()) {
-            throw new ScheduleTitleInvalidException(TimeTableErrorCode.INVALID_TITLE_OR_NAME);
+            throw new ScheduleTitleInvalidException(ErrorCode.INVALID_TITLE_OR_NAME);
         }
     }
 
