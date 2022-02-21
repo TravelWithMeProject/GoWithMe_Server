90b5b7e13fe63494742f038975040b8fe58f6757
노경태 <ilgolc@naver.com>
2022-02-21 15:19:48 +0900

    "수정 하면서 타 도메인 변경 -> 추후 변경 예정"

--- src/main/java/team/backend/goWithMe/domain/mate/domain/persist/Mate.java
+++ src/main/java/team/backend/goWithMe/domain/mate/domain/persist/Mate.java
@@ -1,22 +1,22 @@
-package team.backend.goWithMe.domain.mate.domain.persist;
-
-import javax.persistence.*;
-
-@Entity
-@IdClass(MateId.class)
-public class Mate {
-//   @Id
-//   @ManyToOne
-//   @JoinColumn(name = "user_id")
-//   private User user;
+//package team.backend.goWithMe.domain.mate.domain.persist;
 //
-//   @Id
-//   @ManyToOne(fetch = FetchType.LAZY)
-//   @JoinColumn(name = "mate_id")
-//   private MateList mateList;
+//import javax.persistence.*;
 //
-//   @Id
-//   @ManyToOne(fetch = FetchType.LAZY)
-//   @JoinColumn(name = "trip_id")
-//   private Trip trip;
-}
+//@Entity
+//@IdClass(MateId.class)
+//public class Mate {
+////   @Id
+////   @ManyToOne
+////   @JoinColumn(name = "user_id")
+////   private User user;
+////
+////   @Id
+////   @ManyToOne(fetch = FetchType.LAZY)
+////   @JoinColumn(name = "mate_id")
+////   private MateList mateList;
+////
+////   @Id
+////   @ManyToOne(fetch = FetchType.LAZY)
+////   @JoinColumn(name = "trip_id")
+////   private Trip trip;
+//}
