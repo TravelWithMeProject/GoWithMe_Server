b01823e8aae686c8c0baab2e14b1b8691e689d7a
노경태 <ilgolc@naver.com>
2022-02-21 15:13:16 +0900

    "QueryDSL 추가"

---
+++ src/main/java/team/backend/goWithMe/global/config/QueryDslConfig.java
@@ -0,0 +1,22 @@
+package team.backend.goWithMe.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+@NoArgsConstructor
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(this.entityManager);
+    }
+}
