9cd5076641b04c2a551c5b56d733c5c68b6b6091
Developer_GOLF <77387861+ilgolf@users.noreply.github.com>
2022-03-09 16:17:57 +0900

    "동적 쿼리로 변경 요소가 null일 경우 조건 부 쿼리는 생략된다."

--- src/main/java/team/backend/goWithMe/domain/member/domain/persist/MemberQueryRepository.java
+++ src/main/java/team/backend/goWithMe/domain/member/domain/persist/MemberQueryRepository.java
@@ -1,10 +1,15 @@
 package team.backend.goWithMe.domain.member.domain.persist;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import team.backend.goWithMe.domain.preference.domain.persist.Preference;
+import team.backend.goWithMe.domain.preference.domain.vo.Accommodation;
+import team.backend.goWithMe.domain.preference.domain.vo.PreferenceArrival;
+import team.backend.goWithMe.domain.preference.domain.vo.PreferencePeriod;
 
 import java.util.List;
 
@@ -17,15 +22,40 @@ public class MemberQueryRepository {
     private final JPAQueryFactory query;
 
     // 선호도 조사한 것을 기준으로 검색
-    public List<Member> findAllByFavorite(final Preference memberFavorite, final Pageable pageable) {
+    public List<Member> findAllByFavorite(final Preference memberPreference, final Pageable pageable) {
         return query.selectFrom(member)
-                .leftJoin(member.favorite)
-                .where(member.favorite.favoriteArrival.eq(memberFavorite.getFavoriteArrival())
-                        .or(member.favorite.accommodation.eq(memberFavorite.getAccommodation()))
-                        .or(member.favorite.favoritePeriod.eq(memberFavorite.getFavoritePeriod())))
+                .leftJoin(member.preference)
+                .where(
+                        eqArrival(memberPreference.getPreferenceArrival())
+                        .or(eqAccommodation(memberPreference.getAccommodation()))
+                        .or(eqPeriod(memberPreference.getPreferencePeriod())))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetchResults()
                 .getResults();
+    }
+
+    private BooleanExpression eqPeriod(final PreferencePeriod preferencePeriod) {
+        if (preferencePeriod.startTime() == null || preferencePeriod.endTime() == null) {
+            return null;
+        }
+
+        return member.preference.preferencePeriod.eq(preferencePeriod);
+    }
+
+    private BooleanExpression eqAccommodation(final Accommodation accommodation) {
+        if (accommodation == null) {
+            return null;
+        }
+
+        return member.preference.accommodation.eq(accommodation);
+    }
+
+    private BooleanExpression eqArrival(final PreferenceArrival preferenceArrival) {
+        if (preferenceArrival == null) {
+            return null;
+        }
+
+        return member.preference.preferenceArrival.eq(preferenceArrival);
     }
 }
